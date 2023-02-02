0273b45abc8e4b45c89a68225b425f32f260e8b4
Matthew Grainger <DrMattG@users.noreply.github.com>
2023-02-02 20:24:36 +0100

    delete unused files

--- R/input.R
+++
@@ -1,5 +0,0 @@
-#' @title input
-#' @description A glm model and data object (combined). For testing
-#' @format A glm model object and a data frame
-#' @details data and model just for testing
-"input"
